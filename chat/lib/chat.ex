a6d544af2f612303e6df9bb91741aed2a9e7baa9
eddie <eddievim@foxmail.com>
2022-09-14 00:10:20 +0800

    use Node.self() get name

--- chat/lib/chat.ex
+++ chat/lib/chat.ex
@@ -4,6 +4,17 @@ defmodule Chat do
     IO.puts msg
   end
 
+  def receive_msg_for_toast(msg, from) do
+    IO.puts msg
+    send_msg(from, "eat?")
+  end
+
+  def send_msg(:toast@localhost, msg) do
+    remote_supervisor(:toast@localhost)
+    |> Task.Supervisor.async(__MODULE__, :receive_msg_for_toast, [msg, Node.self()])
+    |> Task.await()
+  end
+
   def send_msg(node, msg) do
     node
     |> remote_supervisor()
